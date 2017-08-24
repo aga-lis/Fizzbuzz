31b1a13b3ac9eaa3c713955174cfa62142c0cfe6
Aga Lis <agnieszkalis@gmail.com>
2017-08-24 17:20:25 +0000

    Adding test for number 5 to fizzbuzz_spec.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" for the number 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" for the number 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
 end
