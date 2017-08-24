e0ac44728a63799e94614b4e9eb6c9c911e0f162
Aga Lis <agnieszkalis@gmail.com>
2017-08-24 17:28:40 +0000

    Adding test for numbers other than 3 and 5 to fizzbuzz_spec.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" for the number 5' do
     expect(fizzbuzz(5)).to eq "buzz"
   end
+  it 'returns number for the number that is not 5 or 3' do
+    expect(fizzbuzz(1)).to eq "1"
+  end
 end
